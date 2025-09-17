bb841dc07cfc18510bdb5c4a5e18b1caa5ce74b1
Henrique Sebastião <contato@henriquesebastiao.com>
2025-09-17 13:30:47 +0000

    Adicionando configurações do Glance

---
+++ update.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# Glance
+cp -r /home/hick/compose/glance/config/glance.yml /home/hick/homelab/containers/glance/config/
+cp -r /home/hick/compose/glance/config/home.yml /home/hick/homelab/containers/glance/config/
+cp -r /home/hick/compose/glance/config/homelab.yml /home/hick/homelab/containers/glance/config/
+cp -r /home/hick/compose/glance/config/projects.yml /home/hick/homelab/containers/glance/config/
+cp -r /home/hick/compose/glance/.env /home/hick/homelab/containers/glance/
+cp -r /home/hick/compose/glance/assets/ /home/hick/homelab/containers/glance/
